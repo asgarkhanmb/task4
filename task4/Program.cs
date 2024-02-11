c4b3eba8a49c6e81a551721311588afb62a9a870
unknown <asgarkhanmb@code.edu.az>
2024-02-11 18:45:02 +0400

    task homework

---
+++ task4/Program.cs
@@ -0,0 +1,53 @@
+﻿//////1. n - ededinin faktorialini hesablayin.
+
+
+//////int n = 8;
+//////int result = 1;	
+
+//////for (int i =n;i>0; i--) 
+//////		{
+//////	result *=i;
+//////}
+//////Console.WriteLine(result);
+
+
+//////2) Verilmis arrayin icerisinde cut ededlerin ceminin kvadratini hesablayin.
+
+////int[] nums = { 1,2,3, 4, 5, 6};
+////int sum = 0;
+
+////for (int i = 0; i < nums.Length; i++)
+////{
+////    if (nums[i] % 2 == 0)
+
+
+////        sum += nums[i];
+
+
+////}
+////   Console.WriteLine(sum*sum);
+
+
+//n en edi gelir. n - ededinin 2-ni hansisa quvvete yukseltdikde alinan edede beraber olub-olmadigini tapin.
+//    Meselen : n ededi 4,8,16 ve s. gelirse yazilsin ki, 2-nin quvettidir. Eks halda quvveti deil yazilsin.
+
+
+//int n = 4;
+//int count = 0;
+
+//for (int i = n; i > 1; i++)
+
+//    if (i % 2 == 0) ;
+
+//{
+//    count++;
+//}
+//{
+//    Console.WriteLine("quvvetidir");
+
+//}
+
+//if (n < 0) ;
+//{
+//    Console.WriteLine("quvveti deyil");
+//}
